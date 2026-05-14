507551a1dcb726641f3d0b44baa6fd9e005b1b3a
Leonardo Fontes <lfontes@fca.uncu.edu.ar>
2026-05-14 11:26:56 +0000

    Ajustar layout del login de ctrl_asis

--- php/login/pant_login.php
+++ php/login/pant_login.php
@@ -3,6 +3,25 @@ class pant_login extends toba_ei_pantalla
 {
 	function generar_layout()
 	{
+		echo "
+		<style type='text/css'>
+			.encabezado,
+			#barra_superior,
+			#barra-superior,
+			.barra-superior-login,
+			#enc-logo,
+			.enc-version,
+			.login-titulo {
+				display: none !important;
+			}
+		</style>";
+
+		echo "<div style='text-align: center; margin-top: 20px; margin-bottom: 30px;'>";
+		echo toba_recurso::imagen_proyecto('logo_grande.gif', true);
+		echo "</div>";
+
+		echo "<div style='text-align: center; margin-top: 30px;'>";
+		echo "<div style='display: inline-block; text-align: left;'>";
 		if ($this->existe_dependencia('seleccion_usuario')) {
 			$this->dep('seleccion_usuario')->generar_html();
 		}
@@ -23,6 +42,9 @@ class pant_login extends toba_ei_pantalla
 			echo '</div>';
 		}
 		echo '</div>';
+		echo "<div style='clear: both;'></div>";
+		echo "</div>";
+		echo "</div>";
 	}
 }
 
